774f8c264a460b0aacb1c7677509d7c2ad2a681b
LiZhengHao <1263212577@qq.com>
2022-11-04 09:39:17 +0800

    优化CAP和特性事务的结合

--- backend_src/MST.Infra.Shared/Attribute/TransactionAttribute.cs
+++ backend_src/MST.Infra.Shared/Attribute/TransactionAttribute.cs
@@ -14,6 +14,10 @@ namespace AspNetCore.StartUpTemplate.Core;
 public class TransactionalAttribute : Rougamo.MoAttribute
 {
     public Propagation Propagation { get; set; } = Propagation.Required;
+    /// <summary>
+    /// 是否使用CAP事务,true的话CAP也加入事务,默认false
+    /// </summary>
+    public bool UseCapTrans { get; set; } = false;
     public IsolationLevel IsolationLevel { get => m_IsolationLevel.Value; set => m_IsolationLevel = value; }
     IsolationLevel? m_IsolationLevel;
 
@@ -23,26 +27,30 @@ public class TransactionalAttribute : Rougamo.MoAttribute
     IUnitOfWork _uow;
     private ICapPublisher _capPublisher;
     private MySqlCapTransaction _capTransaction;
-
+    private bool NeedDisposeCapTrans { get; set; } = true;
     public override void OnEntry(MethodContext context)
     {
         Log.Debug("进入 Transactional事务切面");
-        var uowManager = m_ServiceProvider.Value.GetService<UnitOfWorkManager>();
+        var uowManager = m_ServiceProvider!.Value!.GetService<UnitOfWorkManager>()!;
         _uow = uowManager.Begin(this.Propagation, this.m_IsolationLevel);
         var trans=_uow.GetOrBeginTransaction();
 
         #region CAP相关
-        // 这里接入cap的事务 并且设置cap的自动提交为false
-        _capPublisher = m_ServiceProvider.Value.GetService<ICapPublisher>();
-        _capTransaction = ActivatorUtilities.CreateInstance<MySqlCapTransaction>(m_ServiceProvider.Value);;
-        _capTransaction.AutoCommit = false;
-        _capTransaction.DbTransaction = trans;
-        _capPublisher.Transaction.Value = _capTransaction;
-
+        if (UseCapTrans)
+        {
+            // 这里接入cap的事务 并且设置cap的自动提交为false
+            // ICapPublisher是单例的,里面的是事务是AsyncLocal存储 
+            _capPublisher = m_ServiceProvider!.Value!.GetService<ICapPublisher>()!;
+            if (_capPublisher!.Transaction.Value is  null)
+            {
+                _capTransaction = ActivatorUtilities.CreateInstance<MySqlCapTransaction>(m_ServiceProvider.Value);;
+                _capTransaction.AutoCommit = false;
+                _capTransaction.DbTransaction = trans;
+                _capPublisher.Transaction.Value = _capTransaction;
+            }
+        }
         #endregion
-       
         Log.Debug($"当前事务的guid为{_uow.Orm.Ado.Identifier}");
-        // todo 获取一个mqtransaction并标记开启了事务
     }
     public override void OnExit(MethodContext context)
     {
@@ -55,19 +63,33 @@ public class TransactionalAttribute : Rougamo.MoAttribute
             {
                 if (context.Exception == null)
                 {
-                    Log.Error("UnitofWorkManager 切面 提交 ");
+                    Log.Error("UnitOfWorkManager 切面 提交 ");
                 
                     _uow.Commit();
-                    // _capTransaction?.GetType().GetMethod("Flush", BindingFlags.Instance | BindingFlags.NonPublic)?.Invoke(_capTransaction, null);
+                    if (UseCapTrans)
+                    {
+                        var uowName=_uow.GetType().Name;
+                        // 假提交，CAP先不推消息
+                        if (uowName == "UnitOfWorkVirtual" || uowName == "UnitOfWorkNothing")
+                        {
+                            NeedDisposeCapTrans = false;
+                            return;
+
+                        }
+                        _capTransaction?.GetType().GetMethod("Flush", BindingFlags.Instance | BindingFlags.NonPublic)?.Invoke(_capTransaction, null);
+                    }
                 }
                 else
                 {
-                    Log.Error("UnitofWorkManager 切面 回滚 ");
+                    Log.Error("UnitOfWorkManager 切面 回滚 ");
                     _uow.Rollback();
                 }
             }
             finally
             {
+                if(NeedDisposeCapTrans&&UseCapTrans)
+                    _capTransaction?.Dispose();
+
                 _uow.Dispose();
             }
         }
